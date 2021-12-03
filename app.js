e226dab4af95efdc09038b080e211414b3e9af0b
Josh Fishman <Fishmanjosh251@gmail.com>
2021-12-03 13:52:23 -0600

    finished

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-//CLASS DEFINES BEHAVIORS
-const EventEmitter = require("events");
+const http = require('http')
 
-const Logger = require("./logger");
-const logger = new Logger();
 
-logger.on("messageLogged", (e) => {
-  console.log("Listener Called", e);
-});
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.write('Hello World')
+        res.end()
+    }
+    
+    if(req.url ==='/api/courses'){
+        res.write(JSON.stringify([1,2,3]));
+        res.end()
+    }
+})
 
-logger.log("message");
+
+
+server.listen(3001);
+
+console.log('Listening on port 3001....');
