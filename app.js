34290de6341f5f5dcb55065d51d3955e30ab44f4
Josh Fishman <Fishmanjosh251@gmail.com>
2021-12-03 13:28:59 -0600

    Another

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 //CLASS DEFINES BEHAVIORS
-const { on } = require("events");
-const EventEmitter = require("events");
-const { url } = require("inspector");
+const EventEmitter = require("events");;
 //OBJECT Instance of Class
 const emitter = new EventEmitter();
 
@@ -10,12 +8,7 @@ emitter.on("messageLogged", (e) => {
   console.log("Listener Called", e);
 });
 
-//Raise an Event
-emitter.emit("messageLogged", { id: 1, url: "http://" });
+const log = require('./logger')
+log('message')
 
-//raise: logging (data:message)
-emitter.on("logging", (e) => {
-    console.log("Listener Called", e);
-  });
-
-  emitter.emit("logging", { id: 1, url: "http://" });
+  
